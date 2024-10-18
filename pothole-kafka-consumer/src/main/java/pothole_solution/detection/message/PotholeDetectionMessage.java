8611e7327acb8dc473d17f0e53f40a1074f857d7
donghyuk <donghyuk454@gmail.com>
2024-10-18 18:17:40 +0900

    [POT-70] [feat] 포트홀 탐지 메시지 개발

---
+++ pothole-kafka-consumer/src/main/java/pothole_solution/detection/message/PotholeDetectionMessage.java
@@ -0,0 +1,8 @@
+package pothole_solution.detection.message;
+
+import lombok.ToString;
+import pothole_solution.common.message.BasicMessage;
+
+@ToString
+public class PotholeDetectionMessage extends BasicMessage<PotholeDetectionContent> {
+}
