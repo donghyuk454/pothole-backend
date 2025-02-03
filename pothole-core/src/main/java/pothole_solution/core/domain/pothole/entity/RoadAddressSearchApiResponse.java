7e11c61ee5c14eec811c5e14089ddfb8865eb571
donghyuk <donghyuk454@gmail.com>
2025-02-04 01:12:28 +0900

    [POT-87] [Feat] 응답 결과에 대해 valid, empty 정의

--- pothole-core/src/main/java/pothole_solution/core/domain/pothole/entity/RoadAddressSearchApiResponse.java
+++ pothole-core/src/main/java/pothole_solution/core/domain/pothole/entity/RoadAddressSearchApiResponse.java
@@ -30,4 +30,22 @@ public class RoadAddressSearchApiResponse {
             private String time;
         }
     }
+
+    // 결과가 유효한 지 확인
+    public boolean isInvalid() {
+        return response == null ||
+                response.getStatus() == null ||
+                response.getStatus().equals("ERROR");
+    }
+
+    // 주소 값이 비어 있는 지 확인
+    public boolean isEmptyResult() {
+        return response.getStatus().equals("NOT_FOUND") ||
+                response.getResult() == null ||
+                response.getResult().size() == 0;
+    }
+
+    public RoadAddress getFirstResult() {
+        return response.getResult().get(0);
+    }
 }
