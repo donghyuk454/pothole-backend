d4bcce4a5c67eb9446f04c1d54eb305f6f0cb472
donghyuk <donghyuk454@gmail.com>
2025-03-27 22:43:22 +0900

    [POT-95] [Refactor] 인터페이스 구현 형태로 변경 및 중복 소스 변경

--- pothole-manager-api/src/main/java/pothole_solution/manager/report/repository/ReportRepositoryImpl.java
+++ pothole-manager-api/src/main/java/pothole_solution/manager/report/repository/ReportRepositoryImpl.java
@@ -8,7 +8,6 @@ import com.querydsl.core.types.dsl.StringTemplate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 import pothole_solution.manager.report.dto.RespPotCriteriaCntByPeriodDto;
 import pothole_solution.manager.report.entity.ReportCriteria;
 
@@ -18,20 +17,24 @@ import java.util.List;
 import static pothole_solution.core.domain.pothole.entity.QPothole.pothole;
 
 @Slf4j
-@Repository
 @RequiredArgsConstructor
-public class ReportQueryDslRepository {
+public class ReportRepositoryImpl implements ReportRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
-    public List<RespPotCriteriaCntByPeriodDto> getPotDngrCntByPeriod(LocalDateTime startDate, LocalDateTime endDate,
-                                                                     String queryOfPeriod, ReportCriteria criteria) {
+    @Override
+    public List<RespPotCriteriaCntByPeriodDto> getPotDngrCntByPeriod(LocalDateTime startDate,
+                                                                     LocalDateTime endDate,
+                                                                     String queryOfPeriod,
+                                                                     ReportCriteria criteria) {
 
         NumberPath<Integer> searchCriteria = ReportCriteria.DANGER.equals(criteria) ? pothole.dangerous : pothole.importance;
+
+        StringTemplate periodTemplate = convertDateFormat(queryOfPeriod);
 
         return jpaQueryFactory
                 .select(
                         Projections.constructor(RespPotCriteriaCntByPeriodDto.class,
-                                convertDateFormat(queryOfPeriod),
+                                periodTemplate,
                                 countCriteriaBetween(searchCriteria, 1, 20),
                                 countCriteriaBetween(searchCriteria, 21, 40),
                                 countCriteriaBetween(searchCriteria, 41, 60),
@@ -41,7 +44,7 @@ public class ReportQueryDslRepository {
                 )
                 .from(pothole)
                 .where(pothole.createdAt.between(startDate, endDate))
-                .groupBy(convertDateFormat(queryOfPeriod))
+                .groupBy(periodTemplate)
                 .orderBy(convertDateFormat(queryOfPeriod).asc())
                 .fetch();
     }
@@ -53,10 +56,6 @@ public class ReportQueryDslRepository {
     }
 
     private static NumberExpression<Long> countCriteriaBetween(NumberPath<Integer> criteria, int from, int to) {
-        return Expressions.cases()
-                .when(criteria.between(from, to))
-                .then(1L)
-                .otherwise(0L)
-                .sum();
+        return criteria.between(from, to).count();
     }
 }
