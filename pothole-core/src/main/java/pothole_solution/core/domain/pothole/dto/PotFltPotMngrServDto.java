29f9872dd536312c6d83a0c3bb0968b6f49e5dac
jjunsik <junsik8236@gmail.com>
2025-04-17 21:13:18 +0900

    [POT-92][Improve] roadCode로 조회하기 위해 필터 조회 DTO에 roadCode 필드 추가

--- pothole-core/src/main/java/pothole_solution/core/domain/pothole/dto/PotFltPotMngrServDto.java
+++ pothole-core/src/main/java/pothole_solution/core/domain/pothole/dto/PotFltPotMngrServDto.java
@@ -4,15 +4,20 @@ import lombok.Builder;
 import lombok.Getter;
 import pothole_solution.core.domain.pothole.entity.Progress;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public class PotFltPotMngrServDto {
     private Integer minImportance;
     private Integer maxImportance;
     private final Progress processStatus;
     private final String roadName;
+    private List<String> roadCode = new ArrayList<>();
 
     @Builder
-    public PotFltPotMngrServDto(Integer minImportance, Integer maxImportance, Progress processStatus, String roadName) {
+    public PotFltPotMngrServDto(Integer minImportance, Integer maxImportance, Progress processStatus,
+                                String roadName) {
         this.minImportance = minImportance;
         this.maxImportance = maxImportance;
         this.processStatus = processStatus;
@@ -22,5 +27,9 @@ public class PotFltPotMngrServDto {
     public void changeToAvailableImportance(Integer availableMinImportance, Integer availableMaxImportance) {
         this.minImportance = availableMinImportance;
         this.maxImportance = availableMaxImportance;
+    }
+
+    public void initRoadCode(List<String> roadCode) {
+        this.roadCode = roadCode;
     }
 }
